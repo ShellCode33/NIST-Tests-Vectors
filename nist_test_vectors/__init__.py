2056a8ad1a325b45651736ad6dead3aff8a57ada
ShellCode33 <shellcode33@protonmail.ch>
2021-12-06 22:36:20 +0100

    Add some classes in nist_test_vectors/__init__.py to make them easier to use

--- nist_test_vectors/__init__.py
+++ nist_test_vectors/__init__.py
@@ -3,4 +3,5 @@ import nist_test_vectors.parser
 RspFile = nist_test_vectors.parser.RspFile
 Profile = nist_test_vectors.parser.Profile
 TestVector = nist_test_vectors.parser.TestVector
+TestVectors = nist_test_vectors.parser.TestVectors
 RspParsingError = nist_test_vectors.parser.RspParsingError
